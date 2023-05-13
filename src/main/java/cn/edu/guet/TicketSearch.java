e39b77bac238c6c7dfd622c9873189293ee31f86
张子诚 <2532557832@qq.com>
2023-05-13 20:34:18 +0800

    1.实现了把数据放到了表格里 2.修改了TicketSearch的部分代码使表格起始站终点站里的英文变成中文

--- src/main/java/cn/edu/guet/TicketSearch.java
+++ src/main/java/cn/edu/guet/TicketSearch.java
@@ -52,10 +52,13 @@ public class TicketSearch {
         for (Object item : jsonArr.toArray()) {
             String record = item.toString();
             String[] recordArr = record.split("\\|");
+            String startStation = codeNameMap.get(recordArr[6]);
+            String endStation = codeNameMap.get(recordArr[7]);
+
             Ticket ticket =new Ticket();
             ticket.setTrainNumber(recordArr[3]);
-            ticket.setStartStation(recordArr[6]);
-            ticket.setEndStation(recordArr[7]);
+            ticket.setStartStation(startStation);
+            ticket.setEndStation(endStation);
             ticket.setDepartureTime(recordArr[8]);
             ticket.setArrivalTime(recordArr[9]);
             ticket.setDuration(recordArr[10]);
